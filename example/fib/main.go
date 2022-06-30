44ec7d0d09d32f2cd7d7193065b921496e0e5c6a
xzxiong <xiezexiong@matrixorigin.cn>
2022-06-30 20:39:48 +0800

    stash

--- example/fib/main.go
+++ example/fib/main.go
@@ -15,15 +15,17 @@
 package main
 
 import (
-	"context"
 	//"errors"
+	//"math/rand"
+	"context"
 	"flag"
 	"fmt"
 	"io"
 	"log"
-	//"math/rand"
+	"net/http"
+	_ "net/http/pprof"
 	"os"
-	"os/signal"
+	//"os/signal"
 	"runtime/pprof"
 	"time"
 
@@ -36,6 +38,7 @@ import (
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.10.0"
+	traceCfg "go.opentelemetry.io/otel/trace"
 )
 
 // newExporter returns a console exporter.
@@ -66,15 +69,16 @@ func newResource() *resource.Resource {
 func genCount(ctx context.Context, timeo time.Duration) int64 {
 	var count int64 = 0
 	_3_second := time.Now().Add(timeo)
+	tracer := otel.Tracer("")
 	for !time.Now().After(_3_second) {
-		_, span := otel.Tracer("").Start(ctx, "genCount")
+		_, span := tracer.Start(ctx, "genCount", traceCfg.WithTimestamp(time.Now()))
 		count++
 		/*span.SetAttributes(attribute.String("function", "genCount"))
 		span.SetAttributes(attribute.Int64("index", count))
 		err := errors.New("new error msgs")
 		span.RecordError(err)
 		span.SetStatus(codes.Error, err.Error())*/
-		span.End()
+		span.End(traceCfg.WithTimestamp(time.Now()))
 	}
 	seconds := int64(timeo / time.Second)
 	cntPerSecond := count / seconds
@@ -83,14 +87,22 @@ func genCount(ctx context.Context, timeo time.Duration) int64 {
 	return count
 }
 
+var threadTest *int
+
+func httpGet(w http.ResponseWriter, r *http.Request) {
+	for i := 0; i < *threadTest; i++ {
+		go genCount(context.Background(), 1*time.Second)
+	}
+}
+
 func main() {
 
-	var cpuProfile = flag.String("cpuprofile", "cpu.pprof", "write cpu profile to file")
-	var threadTest = flag.Int("thread", 7, "run gorouting count")
+	threadTest = flag.Int("thread", 7, "run gorouting count")
+	//var cpuProfile = flag.String("cpuprofile", "cpu.pprof", "write cpu profile to file")
 	//var memProfile = flag.String("memprofile", "", "write mem profile to file")
 	flag.Parse()
 	//采样cpu运行状态
-	if *cpuProfile != "" {
+	/*if *cpuProfile != "" {
 		f, err := os.Create(*cpuProfile)
 		if err != nil {
 			log.Fatal(err)
@@ -98,6 +110,7 @@ func main() {
 		pprof.StartCPUProfile(f)
 		defer pprof.StopCPUProfile()
 	}
+	*/
 
 	threadProfile := pprof.Lookup("threadcreate")
 	fmt.Printf("> goroutining  counts: %d\n", threadProfile.Count())
@@ -131,29 +144,34 @@ func main() {
 	}()
 	otel.SetTracerProvider(tp)
 
-	//genCount(context.Background(), 1*time.Second)
-	//genCount(context.Background(), 3*time.Second)
-	//genCount(context.Background(), 30*time.Second)
-	for i := 0; i < *threadTest; i++ {
-		go genCount(context.Background(), 1*time.Second)
-	}
+	http.HandleFunc("/get", httpGet)
+	http.ListenAndServe("localhost:8000", nil)
 
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, os.Interrupt)
-
-	errCh := make(chan error)
-	app := NewApp(os.Stdin, l)
-	go func() {
-		errCh <- app.Run(context.Background())
-	}()
-
-	select {
-	case <-sigCh:
-		l.Println("\ngoodbye")
-		return
-	case err := <-errCh:
-		if err != nil {
-			l.Fatal(err)
+	/*
+		//genCount(context.Background(), 1*time.Second)
+		//genCount(context.Background(), 3*time.Second)
+		//genCount(context.Background(), 30*time.Second)
+		for i := 0; i < *threadTest; i++ {
+			go genCount(context.Background(), 1*time.Second)
 		}
-	}
+
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, os.Interrupt)
+
+		errCh := make(chan error)
+		app := NewApp(os.Stdin, l)
+		go func() {
+			errCh <- app.Run(context.Background())
+		}()
+
+		select {
+		case <-sigCh:
+			l.Println("\ngoodbye")
+			return
+		case err := <-errCh:
+			if err != nil {
+				l.Fatal(err)
+			}
+		}
+	*/
 }
